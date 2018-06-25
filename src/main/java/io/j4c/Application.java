14602fe5f3184137447b1fa3cd2502ad90bd6f4b
Peter Nagy <nagypeter76@gmail.com>
2018-06-25 14:59:24 +0200

    remove lambda dependency

--- src/main/java/io/j4c/Application.java
+++ src/main/java/io/j4c/Application.java
@@ -28,7 +28,7 @@ public class Application extends SpringBootServletInitializer implements WebAppl
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+/*
 	@Bean
 	CommandLineRunner init() {
 
@@ -39,7 +39,7 @@ public class Application extends SpringBootServletInitializer implements WebAppl
 		};
 
 	}
-
+*/
 	@Override
 	public void onStartup(ServletContext container) {
 	}
